b6837b8e5182250d6f403293af060899d4eae5e6
nagarajjayakumar <nagaraj.jayakumar@gmail.com>
2018-05-22 15:01:17 -0400

    Orchestrator

---
+++ src/main/scala/com/hortonworks/faas/spark/predictor/schema_crawler/InferenceEngineOptions.scala
@@ -0,0 +1,41 @@
+package com.hortonworks.faas.spark.predictor.schema_crawler
+
+import com.hortonworks.faas.spark.predictor.inference_engine.analytic.common._
+import com.hortonworks.faas.spark.predictor.inference_engine.configuration.ConfigurationOptionMap
+import com.hortonworks.faas.spark.predictor.inference_engine.task.inference_engine_master
+
+/**
+  * Created by njayakumar on 5/16/2018.
+  */
+class SchemaCrawlerOptions(val task:String, val o:String, val w:String ) extends CommonDataFrameWriterOption(o,w) {
+  def this( t:String, cdfw:CommonDataFrameWriterOption ) {
+    this( t, cdfw.output, cdfw.write_mode)
+  }
+
+  def isValid(): Boolean = {
+    true
+  }
+}
+
+object SchemaCrawlerOptions {
+  val TASK_KEY="task"
+
+  def apply(args: Array[String]): SchemaCrawlerOptions = {
+    apply(ConfigurationOptionMap(args))
+  }
+
+  def apply(options: ConfigurationOptionMap): SchemaCrawlerOptions = {
+
+    val cdfw:CommonDataFrameWriterOption = CommonDataFrameWriterOption(options)
+
+    val t:String = if(options.opts.contains(TASK_KEY) && options.opts(TASK_KEY).nonEmpty) options.opts(TASK_KEY)(0) else inference_engine_master.TASK
+
+    new SchemaCrawlerOptions(t, cdfw)
+  }
+
+  def printUsage(): Unit = {
+    CommonDataFrameWriterOption.printUsage()
+
+    println(s"${TASK_KEY} | Task to perform : { ${inference_engine_master.TASK}, ${inference_engine_master.TASK}, ${inference_engine_master.TASK} }; Default: ${inference_engine_master.TASK}")
+  }
+}
---
+++ src/main/scala/com/hortonworks/faas/spark/predictor/schema_crawler/SchemaCrawler.scala
@@ -0,0 +1,97 @@
+package com.hortonworks.faas.spark.predictor.schema_crawler
+
+import java.sql.Timestamp
+
+import com.hortonworks.faas.spark.connector.hana.util.HanaDbConnectionInfo
+import com.hortonworks.faas.spark.predictor.inference_engine.task.inference_engine_master
+import com.hortonworks.faas.spark.predictor.util._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.joda.time.DateTime
+
+
+/**
+  * Inference Engine to infer the data type from HANA Samples.
+  *
+  * Multiple Sampling method
+  *   1) equally weighted Sample
+  *   2) population size for each group "on the fly":
+  *   3) stratified reservoir sampling
+  *   4) stratified constant proportion
+  *
+  */
+object SchemaCrawler extends ExecutionTiming with Logging
+  with DfsUtils
+  with SparkUtils {
+
+  val dbName: String = {
+    "_SYS_BIC"
+  }
+
+  val masterHost = sys.env.get("HANADB_HOST_TEST").getOrElse("127.0.0.1")
+  //val masterHost = sys.env.get("MYSQLDB_HOST_TEST").getOrElse("127.0.0.1")
+  val masterConnectionInfo: HanaDbConnectionInfo =
+    HanaDbConnectionInfo(masterHost, 30015, "SYS_VDM", "Cnct2VDM4", dbName) // scalastyle:ignore
+  val leafConnectionInfo: HanaDbConnectionInfo =
+    HanaDbConnectionInfo(masterHost, 30015, "SYS_VDM", "Cnct2VDM4", dbName) // scalastyle:ignore
+
+  val local: Boolean = true
+
+  def main(args: Array[String]): Unit = {
+    val opts: SchemaCrawlerOptions = SchemaCrawlerOptions(args)
+
+    if (!opts.isValid()) {
+      SchemaCrawlerOptions.printUsage()
+      System.exit(1)
+    }
+
+    var conf = new SparkConf()
+      .setAppName("HanaDb Connector Test")
+      .set("spark.hanadb.host", masterConnectionInfo.dbHost)
+      .set("spark.hanadb.port", masterConnectionInfo.dbPort.toString)
+      .set("spark.hanadb.user", masterConnectionInfo.user)
+      .set("spark.hanadb.password", masterConnectionInfo.password)
+      .set("spark.hanadb.defaultDatabase", masterConnectionInfo.dbName)
+      .set("spark.driver.host", "localhost")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      //.set("spark.ui.port", (4040 + scala.util.Random.nextInt(1000)).toString)
+
+    if (local) {
+      conf = conf.setMaster("local")
+    }
+
+    val sparkBuilder = createSparkBuilder(
+      s"Inference Engine ",
+      conf,
+      args,
+      6)
+    val spark = sparkBuilder.getOrCreate()
+
+    val current_time: Timestamp = new Timestamp(DateTime.now().toDate.getTime)
+
+    try {
+      val output_df = opts.task match {
+        case inference_engine_master.TASK =>
+          time(s"run task for ${inference_engine_master.TASK}",
+            inference_engine_master.getData(spark, current_time))
+        case _ =>
+          val d:RDD[Row] = spark.sparkContext.parallelize( Seq[Row]( Row.fromSeq(Seq("Unknown task type"))))
+          spark.createDataFrame(d, StructType(StructField("ERROR", StringType, nullable = true) :: Nil))
+
+      }
+
+      output_df.printSchema()
+
+    } finally {
+      //make sure to call spark.stop so the history works
+      time("Stopping Spark", {
+        spark.stop()
+      })
+    }
+
+    logDebug("""Done processing ...""")
+  }
+}
