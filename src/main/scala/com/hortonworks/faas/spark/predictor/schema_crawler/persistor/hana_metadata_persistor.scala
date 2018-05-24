37ea31b8137f86a177a59fff12d0c01aed22e6a7
nagarajjayakumar <nagaraj.jayakumar@gmail.com>
2018-05-23 23:25:47 -0400

    Schema Crawler

--- src/main/scala/com/hortonworks/faas/spark/predictor/schema_crawler/persistor/hana_metadata_persistor.scala
+++ src/main/scala/com/hortonworks/faas/spark/predictor/schema_crawler/persistor/hana_metadata_persistor.scala
@@ -1,11 +1,17 @@
 package com.hortonworks.faas.spark.predictor.schema_crawler.persistor
 
+import java.util.Calendar
+
 import com.hortonworks.faas.spark.predictor.orm.service.Connection
 import com.hortonworks.faas.spark.predictor.orm.setting.DBSettings
-import com.hortonworks.faas.spark.predictor.schema_crawler.model.HanaActiveObject
-import com.hortonworks.faas.spark.predictor.util.Logging
+import com.hortonworks.faas.spark.predictor.schema_crawler.model.{HanaActiveObject, SourceDbActiveObject, SourceDbActiveObjectDetail}
+import com.hortonworks.faas.spark.predictor.util.{CommonData, Logging}
+import com.hortonworks.faas.spark.predictor.xml.models.LogicalModelAttribute
+import com.hortonworks.faas.spark.predictor.xml.parser.XmlParser
 import org.apache.spark.sql.{Dataset, SparkSession}
 import scalikejdbc.{DB, NamedDB}
+
+import scala.xml.XML
 
 class hana_metadata_persistor(val ds: Dataset[HanaActiveObject],
                               val spark: SparkSession,
@@ -22,6 +28,41 @@ class hana_metadata_persistor(val ds: Dataset[HanaActiveObject],
 
   def persist(): Unit = {
     val hao: HanaActiveObject = ds.head
+
+    val haoid = SourceDbActiveObject.createWithAttributes('namespace -> hao.PACKAGE_ID,
+      'dbObjectName -> hao.OBJECT_NAME,
+      'dbObjectType -> hao.OBJECT_SUFFIX,
+      'dbObjectTypeSuffix -> hao.OBJECT_SUFFIX,
+      'version -> 1,
+      'activatedAt -> Calendar.getInstance.getTime,
+      'activatedBy -> System.getProperty("user.name"),
+      'isActive -> true)
+
+    val haodetails = parseMetaDataXml(hao.CDATA.get)
+
+    for (haodetail: LogicalModelAttribute <- haodetails){
+      
+      SourceDbActiveObjectDetail.createWithAttributes(
+        'columnName -> haodetail.id,
+        'haoid -> haoid,
+        'isKey -> haodetail.key,
+        'col_order -> haodetail.order,
+        'attributeHierarchyActive -> haodetail.attributeHierarchyActive,
+        'displayAttribute -> haodetail.displayAttribute,
+        'defaultDescription -> haodetail.logicalModelAttributesAttribDesc.head.defaultDescription,
+        'sourceObjectName -> haodetail.logicalModelAttributesAttribKeyMapping.head.columnObjectName,
+        'sourceColumnName ->  haodetail.logicalModelAttributesAttribKeyMapping.head.columnName,
+        'isRequiredForFlow -> true)
+
+    }
+
+
+  }
+
+  def parseMetaDataXml(xmlString: String): Array[LogicalModelAttribute] = {
+    val xmlSource = XML.loadString(xmlString)
+    val logicalModelAttribute = XmlParser.parse(xmlSource)(LogicalModelAttribute.xmlRead)
+    logicalModelAttribute.toArray
   }
 
 }
