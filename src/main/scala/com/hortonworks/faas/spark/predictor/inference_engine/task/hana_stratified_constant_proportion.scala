4d373d5b73267e66345774ef1b880d46d1259556
nagarajjayakumar <nagaraj.jayakumar@gmail.com>
2018-05-27 17:21:05 -0400

    Inference Engine

--- src/main/scala/com/hortonworks/faas/spark/predictor/inference_engine/task/hana_stratified_constant_proportion.scala
+++ src/main/scala/com/hortonworks/faas/spark/predictor/inference_engine/task/hana_stratified_constant_proportion.scala
@@ -2,46 +2,50 @@ package com.hortonworks.faas.spark.predictor.inference_engine.task
 
 import java.sql.Timestamp
 
+import com.google.common.base.Joiner
+import com.hortonworks.faas.spark.connector.util.InferSchema
 import com.hortonworks.faas.spark.predictor.inference_engine.InferenceEngineOptions
 import com.hortonworks.faas.spark.predictor.mdb.model.SourceDbActiveObjectDetail
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import com.google.common.base.Joiner
-import com.hortonworks.faas.spark.connector.util.InferSchema
 import org.apache.spark.sql.types.StructType
 
-object hana_stratified_reservoir_sampler {
+object hana_stratified_constant_proportion {
 
 
-  val TASK: String = "hana_stratified_reservoir_sampler"
+
+  val TASK: String = "hana_stratified_constant_proportion"
 
   /*
-  SELECT MANDT, SPRAS, GNTYP, GNTXT
-FROM
-(
-    SELECT *, ROW_NUMBER() OVER (PARTITION BY MANDT ORDER BY rnd) as rnk
-    FROM (
-        SELECT MANDT, SPRAS,GNTYP,GNTXT, RAND() AS rnd
-        FROM  SLTECC.T352T_T
-    ) bucketed
-) sampled
--- assuming we want 3 records from each group
-WHERE rnk <= 3
+
+  if we assume or confirm that there are more than 100 records in each group,
+  then we can select the X% records from each group in a single pass using NTILE function as shown below.
+
+      SELECT MANDT, SPRAS, GNTYP, GNTXT
+    FROM
+    (
+        SELECT  *, NTILE(100) OVER (PARTITION BY MANDT ORDER BY rnd) as tile
+        FROM (
+            SELECT MANDT, SPRAS,GNTYP,GNTXT, RAND() AS rnd
+            FROM  SLTECC.T352T_T
+        ) bucketed
+    ) sampled
+    -- assuming we want 10% records from each letter group
+    WHERE tile <= 10
    */
 
 
-
   def getData(spark: SparkSession,
-                  opts: InferenceEngineOptions,
-                  dbaoDetails : List[SourceDbActiveObjectDetail],
-                  keys: List[SourceDbActiveObjectDetail],
-                  current_time: Timestamp): DataFrame = {
+              opts: InferenceEngineOptions,
+              dbaoDetails : List[SourceDbActiveObjectDetail],
+              keys: List[SourceDbActiveObjectDetail],
+              current_time: Timestamp): DataFrame = {
 
 
     val keysAsCsv: String = getKeysAsCsv(keys)
     val hana_sampling_query: String = s"select * from (" +
-      s"                                     select *, ROW_NUMBER() OVER (PARTITION BY ${keysAsCsv} ORDER BY rnd) as rnk FROM ( " +
+      s"                                     SELECT  *, NTILE(${opts.sampling_size}) OVER (PARTITION BY ${keysAsCsv} ORDER BY rnd) as tile FROM ( " +
       s"                                            SELECT *, RAND() AS rnd  FROM  '${opts.src_dbo_name}'   ) bucketed" +
-      s"                                     ) sampled where rnk <= ${opts.sampling_size}  "
+      s"                                     ) sampled where tile <= ${opts.sampling_percentage}  "
 
     val sql = hana_sampling_query
     val df = spark
